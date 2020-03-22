97e71d6c3cdeb0343fa1a2d8d7febdd03ee07d9c
lequangtruongg <55335622+lequangtruongg@users.noreply.github.com>
2020-03-22 16:42:44 +0700

    bài tập tuần 2

---
+++ Tuần 2/Interface/Interface/IHuman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IHuman
+    {
+        void Speak(string Message);
+        string Name
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Tuần 2/Interface/Interface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHuman h;
+            Theodore t;
+            h = new Theodore();
+            //cast for assignement  
+            t = (Theodore)h;
+            //use class based reference 
+            t.Name = "Fred";
+            t.Speak("I like public implementations!");
+            //use interface reference  
+            h.Name = "Tin";
+            h.Speak("Tin Bile <3");
+            Console.ReadLine();
+        }
+    }
+}
